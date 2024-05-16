1d783f66daa1a4b09774dbec5f1c7b13bea6eb47
JPSamy <167424333+JPSamy@users.noreply.github.com>
2024-05-16 16:10:01 +0800

    Update HW1.py

--- HW1.py
+++ HW1.py
@@ -1,15 +1,16 @@
 import feedparser
 import pprint
 # Parse the RSS feed
-feed1 = feedparser.parse('https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=1&Suburb=maddington&Day=today')
-
-feed2 = feedparser.parse('https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=1&Suburb=maddington&Day=tomorrow')
-
-
+feed1 = feedparser.parse('https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=1&Day=tomorrow')  
  
-#pprint.pprint(feed1)
-#pprint.pprint(feed2)
-# Begin HTML content
+feed2 = feedparser.parse('https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=1&Day=today')
+
+today_tomorrow = feed1['entries'] + feed2 ['entries']
+ 
+sorted_entries = sorted(today_tomorrow ,key=lambda entry: float(entry['price']))
+
+#pprint.pprint(today_tomorrow) 
+
 html_content = """
 <!DOCTYPE html>
 <html lang="en">
@@ -33,53 +34,47 @@ html_content = """
     </style>
 </head>
 <body>
-    <h2>Fuel Prices in Maddington Today</h2>
+    <h2>Fuel Prices</h2>
     <table>
         <tr>
+            <th>day</th>
+            <th>location</th>
             <th>Address</th>
             <th>Price today</th>
             <th>Brand</th>
+        
         </tr>
+        
 """
-
-# Loop through each entry and add rows to the table
-for entry in feed1['entries']:
+for entry in sorted_entries:
+    day = "Tomorrow" if entry in feed1['entries'] else "Today"
+    
+    location = entry['location']
     address = entry['address']
     price = entry['price']
     brand = entry['brand']
     # Append row to HTML content
     html_content += f"""
         <tr>
+            <td>{day}</td>
+            <td>{location}</td>
             <td>{address}</td>
             <td>{price}</td>
             <td>{brand}</td>
         </tr>
     """
 
-html_content += """    </table>
+html_content += """    
+
+</table>
     <table>
     <tr>
             <th>Address</th>
-            <th>Price tomorrow</th>
+            <th>Price </th>
             <th>Brand</th>
         </tr>
 """
 
-# Loop through each entry and add rows to the table
-for entry in feed2['entries']:
-    address = entry['address']
-    price = entry['price']
-    brand = entry['brand']
-    # Append row to HTML content
-    html_content += f"""
-        <tr>
-            <td>{address}</td>
-            <td>{price}</td>
-            <td>{brand}</td>
-        </tr>
-    """
-
-# End HTML content
 html_content += """
     </table>
 </body>
@@ -87,7 +82,7 @@ html_content += """
 """
 
 # Write the HTML content to a file
-with open('fuel_prices1.html', 'w') as f:
+with open('fuel_prices3.html', 'w') as f:
     f.write(html_content)
 
 print("HTML file created successfully.")
